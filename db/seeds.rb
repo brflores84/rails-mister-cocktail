db048bd88e8690406c082c71921b6b327d9652e1
Bruno Flores <brunogfloresvela@gmail.com>
2019-02-14 17:27:08 +0100

    drinks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+require 'json'
+puts "Deleting DB....."
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts 'Creating 10 fake cocktails...'
+
+10.times do
+  cocktail = Cocktail.new(
+    name: Faker::Space.unique.star
+  )
+  cocktail.save!
+end
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+user_serialized = open(url).read
+result = JSON.parse(user_serialized)
+  result['drinks'].each do |item|
+    Ingredient.create!(name: item["strIngredient1"])
+    # p item['strIngredient1']
+  end
+
+puts 'Finished!'
